14b75a23184e58b1412d0909e6da85e091177f8e
Aaron Hebson <Aarommh7@gmail.com>
2020-05-10 16:33:24 -0400

    Created route for the add sections component.

--- flask-vue-crud/server/app.py
+++ flask-vue-crud/server/app.py
@@ -11,10 +11,37 @@ app.config.from_object(__name__)
 # enable CORS
 CORS(app, resources={r'/*': {'origins': '*'}})
 
+# add predefined sections for now
+SECTIONS = [
+  {
+    'title': 'Web Development',
+    'author': 'Aaron Hebson',
+    'purpose': 'A place for all web development read articles',
+  },
+  {
+    'title': 'History',
+    'author': 'Aaron Hebson',
+    'purpose': 'A place to store any articles about history at.',
+  },
+  {
+    'title': 'Economics',
+    'author': 'Aaron Hebson',
+    'purpose': 'A place to store any articles about economics at.',
+  },
+]
+
 # sanity check route
 @app.route('/ping', methods=['GET'])
 def ping_pong():
   return jsonify('pong!')
+
+# sanity check route
+@app.route('/sections', methods=['GET'])
+def all_sections():
+  return jsonify({
+    'status': 'success',
+    'sections': SECTIONS,
+  })
 
 if __name__ == '__main__':
   app.run()
